4b2990778b32c0255d8694151618787b88271549
jinsoo Kim <jinsoo335@naver.com>
2022-11-10 15:37:52 +0900

    Update Create Alert API

--- src/main/java/com/example/demo/src/Alerts/AlertService.java
+++ src/main/java/com/example/demo/src/Alerts/AlertService.java
@@ -5,6 +5,7 @@ import com.example.demo.security.JwtTool;
 import com.example.demo.src.Alerts.model.PostCreateAlertReq;
 import com.example.demo.src.Alerts.model.PostCreateAlertRes;
 import com.example.demo.src.Folders.FolderProvider;
+import com.example.demo.src.Links.LinkProvider;
 import com.example.demo.src.Users.UserProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,13 +18,15 @@ public class AlertService {
     private final AlertDao alertDao;
     private final AlertProvider alertProvider;
 
+    private final LinkProvider linkProvider;
     private final FolderProvider folderProvider;
     private final UserProvider userProvider;
 
     @Autowired
-    public AlertService(AlertDao alertDao, AlertProvider alertProvider, FolderProvider folderProvider, UserProvider userProvider) {
+    public AlertService(AlertDao alertDao, AlertProvider alertProvider, LinkProvider linkProvider, FolderProvider folderProvider, UserProvider userProvider) {
         this.alertDao = alertDao;
         this.alertProvider = alertProvider;
+        this.linkProvider = linkProvider;
         this.folderProvider = folderProvider;
         this.userProvider = userProvider;
     }
@@ -57,10 +60,15 @@ public class AlertService {
         // 폴더 보낼 경우 자신의 폴더가 맞는지 확인
         if(postCreateAlertReq.getAlertType() == 1
                 && folderProvider.checkFolder(sendUserIdx, postCreateAlertReq.getFolderIdx()) == 0){
-            throw new BaseException(FOLDERS_NOT_EXIST_FOLDER);
+            throw new BaseException(FOLDERS_NOT_HAVE_USERS);
         }
 
         // 링크 보낼 경우 자신의 링크가 맞는지 확인
+        if(postCreateAlertReq.getAlertType() == 2
+                && linkProvider.checkLinkUser(sendUserIdx, postCreateAlertReq.getLinkIdx()) == 0){
+            throw new BaseException(LINKS_NOT_HAVE_USERS);
+        }
+
 
         try{
             PostCreateAlertRes postCreateAlertRes = alertDao.createAlert(sendUserIdx, postCreateAlertReq);
