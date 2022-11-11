4fc9dc9c575bb59670d67e511da09c85ba8cab4f
jinsoo Kim <jinsoo335@naver.com>
2022-11-11 17:13:05 +0900

    Fix Shell Script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -10,8 +10,8 @@ TIME_NOW=$(date +%c)
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다"
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션이 종료 " >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션이 종료 "
   kill -15 $CURRENT_PID
 fi
