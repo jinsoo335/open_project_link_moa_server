8baafb2db8039339a24264b3ec95863e2c8f00fb
jinsoo Kim <jinsoo335@naver.com>
2022-11-11 19:14:07 +0900

    Fix start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY=/home/ubuntu/action
 
-./gradlew clean build
+./$REPOSITORY/gradlew clean build
 
 echo "> 새 어플리케이션 배포"
-nohup java -jar build/libs/demo-0.0.1-SNAPSHOT.jar >> $REPOSITORY/deploy.log &
+nohup java -jar $REPOSITORY/build/libs/demo-0.0.1-SNAPSHOT.jar >> $REPOSITORY/deploy.log &
