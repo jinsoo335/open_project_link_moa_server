4fa76c4ba80a16e8b6ec100557f63a72bb778aca
jinsoo Kim <jinsoo335@naver.com>
2022-11-11 19:10:01 +0900

    Add nohup output to deploy.log

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ REPOSITORY=/home/ubuntu/action
 ./gradlew clean build
 
 echo "> 새 어플리케이션 배포"
-nohup java -jar $REPOSITORY/build/libs/demo-0.0.1-SNAPSHOT.jar >> $REPOSITORY/deploy.log &
+nohup java -jar build/libs/demo-0.0.1-SNAPSHOT.jar >> $REPOSITORY/deploy.log &
