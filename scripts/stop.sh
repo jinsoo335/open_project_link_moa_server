21561e21315e0e4e570cb19bd84ea8e5e79caeed
jinsoo Kim <jinsoo335@naver.com>
2022-11-11 19:53:44 +0900

    Fix stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
 PROJECT_ROOT="/home/ubuntu/action"
-JAR_FILE="/build/libs/demo-0.0.1-SNAPSHOT.jar"
 
 
-TIME_NOW=$(date +%c)
+BUILD_JAR=$(ls /home/ubuntu/action/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다"
+if [ -z $CURRENT_PID ]
+then
+  echo "현재 실행중인 애플리케이션이 없습니다"
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션이 종료 "
+  echo "실행중인 $CURRENT_PID 애플리케이션이 종료 "
   kill -15 $CURRENT_PID
 fi
