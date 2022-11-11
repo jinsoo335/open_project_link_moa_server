a6d1f1c73307c5f96f9c4ef71fa10c4c88a33eaa
jinsoo Kim <jinsoo335@naver.com>
2022-11-11 19:48:19 +0900

    Add grant .jar

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,9 +4,8 @@ REPOSITORY=/home/ubuntu/action
 
 chmod -R 777 $REPOSITORY
 
-
 sh $REPOSITORY/gradlew clean build
 
-
+chmod +x $REPOSITORY/build/libs/demo-0.0.1-SNAPSHOT.jar
 echo "> 새 어플리케이션 배포"
 nohup java -jar $REPOSITORY/build/libs/demo-0.0.1-SNAPSHOT.jar >> $REPOSITORY/deploy.log &
